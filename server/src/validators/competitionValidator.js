1e96637256e0246f598a8eb9d904cead63a3f53a
Maadhav27-B <boopathymaadhav@gmail.com>
2026-09-22 20:54:33 +0530

    Add server/src/validators/competitionValidator.js

---
+++ server/src/validators/competitionValidator.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const validateObjectId = (paramName = 'id') => {
+  return (req, res, next) => {
+    const id = req.params[paramName];
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: `Invalid ${paramName} format`,
+        code: 'INVALID_ID_FORMAT'
+      });
+    }
+    next();
+  };
+};
+
+module.exports = {
+  validateObjectId
+};

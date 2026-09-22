d72415545a5417bb71986b9d7fc90f5066cc4d2f
Maadhav27-B <boopathymaadhav@gmail.com>
2026-09-22 20:54:44 +0530

    Add web/vite.config.js

---
+++ web/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web'
+    }
+  }
+});
